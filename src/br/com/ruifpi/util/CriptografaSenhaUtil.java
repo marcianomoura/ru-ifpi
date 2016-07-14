5aa2fb086144234cb781627f475bf62356107cfb
root <root@root-PC>
2016-07-13 21:55:53 -0300

    Classe Utilitária que aplica hash as senhas do usuário

--- src/br/com/ruifpi/util/CriptografaSenhaUtil.java
+++ src/br/com/ruifpi/util/CriptografaSenhaUtil.java
@@ -4,14 +4,12 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import br.com.ruifpi.models.Funcionario;
-
 public class CriptografaSenhaUtil {
 
 	
-	public static String criptografaSenha(Funcionario funcionario)	throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	public static String criptografaSenha(String senha)	throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		MessageDigest algoritmo = MessageDigest.getInstance("SHA-256");
-		byte messageDigest[] = algoritmo.digest(funcionario.getSenha().getBytes("UTF-8"));
+		byte messageDigest[] = algoritmo.digest(senha.getBytes("UTF-8"));
 		StringBuilder builder = new StringBuilder();
 		for (byte b : messageDigest) {
 			builder.append(String.format("%02X", 0xFF & b));
